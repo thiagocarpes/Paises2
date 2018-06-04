256b3607b3e314ec34b8b004a95f84a2a7953813
thiagocarpes <thiagocarpes@gmail.com>
2018-06-04 09:16:56 -0300

    Fragment

--- app/src/main/java/br/usjt/paises/DetalhePaises.java
+++ app/src/main/java/br/usjt/paises/DetalhePaises.java
@@ -2,6 +2,7 @@ package br.usjt.paises;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
@@ -11,6 +12,19 @@ public class DetalhePaises extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalhe_paises);
+
+        android.support.v4.app.FragmentManager fm  = getSupportFragmentManager();
+
+        if (savedInstanceState == null){
+            //começa a transação
+            FragmentTransaction ft = fm.beginTransaction();
+            DadosPais dp = new DadosPais();
+            //adiciona
+            ft.add (R.id.layoutPais, dp, "DadosPaises");
+            //confirma e fecha a transação
+            ft.commit();
+        }
+
         TextView txtPais = (TextView)findViewById(R.id.txtPais);
         Intent intent = getIntent();
         Pais pais = (Pais)intent.getSerializableExtra(Lista_paises.PAIS);
